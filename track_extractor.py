454d32a325fdb4de56bc0ce81785f5ba7f01c9e2
RuizLorenzoChavez <ruiz_chavez@dlsu.edu.ph>
2022-10-09 16:48:43 +0800

    add feature to extract user playlists

--- track_extractor.py
+++ track_extractor.py
@@ -22,8 +22,27 @@ auth_manager = SpotifyClientCredentials(client_id=client_id,
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
 
-#  The Longest Playlist on Spotify (Official)
-playlist_uri = "spotify:playlist:5S8SJdl1BDc0ugpkEvFsIL"
+#  TODO: if we want to increase number of playlist extracted, we can manually collect usernames and extract their playlists
+
+#  10k long playlists as default
+#  will add more playlist URIs
+playlists_uri = ["spotify:playlist:5S8SJdl1BDc0ugpkEvFsIL",
+                "spotify:playlist:6yPiKpy7evrwvZodByKvM9"
+                ]
+
+playlists = spotify.user_playlists('spotify')
+
+#  extracting playlists from Spotify
+print("Adding playlist URIs from Spotify")
+
+while playlists:
+    for playlist in playlists['items']:
+        playlists_uri.append(playlist['uri'])
+    if playlists['next']:
+        playlists = spotify.next(playlists)
+    else:
+        playlists = None
+        print(f"Extraction completed: {len(playlists_uri)} playlists")
 
 #  list of extracted songs
 tracks_list = []
@@ -71,30 +90,43 @@ def extract_track(index) -> dict:
         
     return track_features_dict
 
-#  go through all the pages of the playlist 
-offset = 0
-while True:
-    playlist = spotify.playlist_items(playlist_id=playlist_uri, limit=100, offset=offset)
-    items = playlist["items"]
-    next = playlist["next"]
+#  go through all the playlists available
+for i, playlist_uri in enumerate(playlists_uri):
     
-    for i, item in enumerate(items):
-        tracks_list.append(extract_track(i))
-        print(f"Track {i + offset}: {items[i]['track']['name']} is done")
+    #  to get the title of the playlist
+    playlist_name = spotify.playlist(playlist_id=playlist_uri, fields="name")["name"]
+    
+    print(f"Start playlist #{i} of {len(playlist_uri)}: {playlist_name}")
+    
+    #  go through all the pages of the playlist 
+    offset = 0
+
+    while True:
+        playlist = spotify.playlist_items(playlist_id=playlist_uri, limit=100, offset=offset)
+        items = playlist["items"]
+        next = playlist["next"]
         
-    #  this ends the loop
-    #  if there is no more next url then we've reached the end
-    if next is None:
-        break
+        for i, item in enumerate(items):
+            tracks_list.append(extract_track(i))
+            print(f"Track {i + offset}: {items[i]['track']['name']} is done")
+            
+        #  this ends the loop
+        #  if there is no more next url then we've reached the end
+        if next is None:
+            break
+        
+        #  so that we can move on from the 100-song limit 
+        offset += 100
+        
+        #  let the program rest
+        time.sleep(5)
     
-    #  so that we can move on from the 100-song limit 
-    offset += 100
-    
-    #  let the program rest
-    time.sleep(5)
+    print(f"End: {playlist_name}")
+
+print(f"Extraction process completed: {len(tracks_list)} total tracks")
 
 #  writing the results to a json file
 tracks_json = json.dumps(tracks_list)
 
-with open("tracks.json", "a") as tracks:
+with open("data\\tracks.json", "a") as tracks:
     tracks.write(tracks_json)
