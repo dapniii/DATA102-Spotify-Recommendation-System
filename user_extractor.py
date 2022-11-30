5255b792b284a229e599cc4935a5355204307a4d
RuizLorenzoChavez <ruiz_chavez@dlsu.edu.ph>
2022-11-30 17:06:58 +0800

    user data extractor python scripts created

--- user_extractor.py
+++ user_extractor.py
@@ -1,7 +1,6 @@
 import spotipy
 import os
 import json
-
 from spotipy.oauth2 import SpotifyOAuth
 
 #  calling spotify api keys from local drive
@@ -16,16 +15,65 @@ client_secret = key["data102"]["client_secret"]
 #  scope set to user-top-read
 scope = "user-top-read"
 
-# configuring Spotify client credentials
+# configuring Spotify client credentials + to get current user data
 auth_manager = SpotifyOAuth(client_id=client_id,
                             client_secret=client_secret,
                             redirect_uri="http://localhost:8888/callback",
-                            username="aexylian",
                             scope=scope,
                             requests_timeout=10
                             )
 
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-user_top_tracks = spotify.current_user_top_tracks(limit=1, offset=0, time_range="short_term")["items"][0]
-print(user_top_tracks["name"])
+#  retrieving top 50 most played songs of user
+user_top_tracks = spotify.current_user_top_tracks(limit=50, offset=0, time_range="long_term")["items"]
+
+#  list of tracks + features in dictionary form
+tracks_list = []
+
+#  get features of each track in the top 50
+for i, track in enumerate(user_top_tracks):
+    
+    track_features_dict = {}
+    
+    song_title = track["name"]
+    artist_name = track["artists"][0]["name"]
+    album_title = track["album"]["name"] 
+    release_date = track["album"]["release_date"]
+    duration = track["duration_ms"]
+    popularity =  track["popularity"]
+    uri = track["uri"]
+    
+    song_features_keys = ["song_title",
+                         "artist_name",
+                         "album_title",
+                         "release_date",
+                         "duration",
+                         "popularity",
+                         "uri"
+                         ]
+    
+    song_features_values = [song_title,
+                            artist_name,
+                            album_title,
+                            release_date,
+                            duration,
+                            popularity,
+                            uri
+                            ]
+    
+    for feat_key, feat_values in zip(song_features_keys, song_features_values):
+        track_features_dict[feat_key] = feat_values
+        
+    tracks_list.append(track_features_dict)
+
+#  get username
+user_name = spotify.current_user()["id"]
+
+#  convert current tracks_list to json format
+#  write extracted tracks to json for each user
+tracks_json = json.dumps(tracks_list)
+
+with open(f"data\\users\\{user_name}_tracks.json", "w") as tracks:
+    tracks.write(tracks_json)
+
---
+++ user_feature_extractor.py
@@ -0,0 +1,111 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import os
+import json
+
+#  calling spotify api keys from local drive
+api_path = f'{os.getenv("API")}\\spotify.json'
+
+with open(api_path) as api:
+    key = json.load(api)
+
+client_id = key["data102"]["client_id"]
+client_secret = key["data102"]["client_secret"]
+
+
+# configuring Spotify client credentials
+auth_manager = SpotifyClientCredentials(
+    client_id=client_id, client_secret=client_secret, requests_timeout=10
+)
+spotify = spotipy.Spotify(auth_manager=auth_manager, requests_timeout=120)
+
+
+#  reading in tracks.json
+with open("data\\users\\aexylian_tracks.json") as tracks:
+    tracks_list = json.load(tracks)
+
+#  get the URIs of all the songs in the tracks_list
+uri_list = []
+
+print("Extracting track URIs from list")
+
+for i, track in enumerate(tracks_list):
+    uri = tracks_list[i]["uri"]
+    uri_list.append(uri)
+
+print(f"Extraction completed: {len(uri_list)} total tracks ")
+
+
+def audio_feat_extractor(index) -> dict:
+    """Extracts the audio features from each track.
+
+    Args:
+        index (int): index of the specific track from the list to
+        be extracted from.
+
+    Returns:
+        dict: complete dictionary of extracted data from each track.
+    """
+    audio_feature_dict = {}
+
+    track_instance = extracted_af_list[index]
+
+    audio_features = [
+        "uri",
+        "danceability",
+        "energy",
+        "key",
+        "loudness",
+        "mode",
+        "speechiness",
+        "acousticness",
+        "instrumentalness",
+        "liveness",
+        "valence",
+        "tempo",
+        "time_signature",
+    ]
+
+    #  extract data feature by feature
+    for feature in audio_features:
+        audio_feature_dict[feature] = track_instance[feature]
+
+    return audio_feature_dict
+
+
+#  list of audio features for each track
+audio_feature_list = []
+
+end = 100
+start = 0
+
+print("Audio feature extraction process START")
+
+while True:
+
+    #  per 100 batches of uri
+    trunc_list = uri_list[start:end]
+
+    #  retrieving data through the api
+    extracted_af_list = spotify.audio_features(trunc_list)
+
+    for i, instance in enumerate(extracted_af_list):
+        try:
+            uri_ref = extracted_af_list[i]["uri"]
+            audio_feature_list.append(audio_feat_extractor(i))
+            print(f"Track {i + start}: {uri_ref} is done")
+        except TypeError:
+            print(f"Track {i + start} is unavailable")
+
+    if end >= len(uri_list):
+        print("Audio feature extraction process END")
+        break
+
+    end += 100
+    start = end - 100
+
+#  writing the results to a json file
+audio_features_json = json.dumps(audio_feature_list)
+
+with open("data\\users\\aexylian_features.json", "a") as tracks:
+    tracks.write(audio_features_json)
